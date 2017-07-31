64325cc849d174919ec5f6c0144d3ff239d82d90
Anton Tarutin <anton.tarutin@defa.ru>
2017-07-31 14:18:47 +0300

    odis check parse block, property

--- parse_html/parse_html.php
+++ parse_html/parse_html.php
@@ -6,18 +6,58 @@
  * Time: 20:14
  */
 
+function valBlock($tag, $valBlock, $needblock){
+    $allPropTag = explode(" ",$tag);
+    $TegName = $allPropTag[0];
+    foreach ($needblock as $needName){
+        if($TegName == $needName){
+            $valBlock = 1;
+        }
+
+        if($TegName == "/".$needName){
+            if ($valBlock == 1){
+                $valBlock = 2;
+            }else{
+                $valBlock = 0;
+            }
+        }
+    }
+
+    return $valBlock;
+}
+
 function parsePropertyTag($key, $tag){
     $allPropTag = explode(" ",$tag);
     $TegName = $allPropTag[0];
     preg_match_all('/[ ]+([^= ]+)[="]+([\w- ]+)["]+/i', $tag, $property);
-    print_r($property);
 
 }
 
-function parseHtml($html){
+function NewBlok(){
+    echo "\n";
+}
+
+function getBlok($tag, $valBlock){
+    if(($valBlock == 1) || ($valBlock == 2)){
+        print_r($tag);
+    }
+    if($valBlock == 2){
+        NewBlok();
+        $valBlock = 0;
+    }
+    return $valBlock;
+}
+
+function parseHtml($html, $needblock){
     preg_match_all ( '/<([^>]+)>/i' , $html , $tags); //все теги
 
+    $valBlock = 0;
+
     foreach ($tags[1] as $key=>$tag){
+        $valBlock = valBlock($tag, $valBlock, $needblock);
+
+        $valBlock = getBlok($tag, $valBlock);
+
         if (count(explode(" ",$tag)) > 1){
             parsePropertyTag($key, $tag);
         }
@@ -27,12 +67,9 @@ function parseHtml($html){
 
 $html = file_get_contents("http://university.netology.ru/user_data/tarutin/bitrix/index.html");
 
+$needblock = ["form", "nav"];
 
-//$dom = new DOMDocument;
-//
-//$dom->loadHTML($html);
-
-parseHtml($html);
+parseHtml($html, $needblock);
 
 
 //preg_match_all ( '/<([^>]+)>/i' , $html , $tags); //все теги без значений
