852ce21396342b7067388c7ce5a2be793b24b496
Anton Tarutin <anton.tarutin@defa.ru>
2017-08-04 15:50:42 +0300

    bugs files in fi_row non class equals

---
+++ create_Need_files/create_file_row.php
@@ -0,0 +1,122 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anton.tarutin
+ * Date: 04.08.2017
+ * Time: 10:18
+ */
+require_once('block_for_row.php');
+
+function isBiblock($tag, $renameTags){
+    preg_match_all('/class[="]+([\w]+)/i', $tag, $tagRow);
+    if(isset($tagRow[1][0])){
+        if(in_array($tagRow[1][0], $renameTags->biForRow)){
+            return 1;
+        }
+        return 0;
+    }
+
+
+}
+
+function oneBlock($tags, $renameTags, $keyAnotherBlock){
+    $scopeTags = [];
+    $treeTags = [];
+
+    for ( ; ;$keyAnotherBlock++){
+
+        $tag = $tags[$keyAnotherBlock];
+
+
+        preg_match_all('/<([\w\S]+)[ >]+/i', $tag, $nameTag);
+
+
+        $isBiblock = isBiblock($tag, $renameTags);
+
+
+
+        if ($isBiblock == 1){
+            array_push($scopeTags, $nameTag[1][0]);
+            array_push($treeTags, $tag);
+            continue;
+        }
+
+
+        if(count($scopeTags) > 0){
+
+            preg_match_all('/[\w\S]/i',$nameTag[1][0], $nameTagSymbol);
+            if ($nameTagSymbol[0][0] == "/"){
+                array_pop($scopeTags);
+
+            }else{
+                if(!in_array($nameTag[1][0], $renameTags->isSoloTag)){
+                    array_push($scopeTags, $nameTag[1][0]);
+                }
+
+            }
+            array_push($treeTags, $tag);
+        }
+
+        if((count($scopeTags) == 0) && ($isBiblock == 0)){
+            return $treeTags;
+        }
+
+    }
+
+}
+
+function keyAnoutherBlock($tags, $oneBlock, $keyAnotherBlock){
+    $keyOneBlock = 0;
+
+    for ( ; $keyAnotherBlock < count($tags)-1; $keyAnotherBlock++){
+        $tag = $tags[$keyAnotherBlock];
+
+        if(!isset($oneBlock[$keyOneBlock])){
+            $keyOneBlock = 0;
+        }
+
+        if($oneBlock[$keyOneBlock] == $tag){
+            echo "\n".$tag;    //give all like blocks
+
+        }else{
+            ///изменить на сравнение по классам
+            return $keyAnotherBlock; //the last key of another block
+        }
+
+        $keyOneBlock ++;
+
+    }
+
+    return $keyAnotherBlock;
+}
+
+function ctartContentFile($tags, $renameTags){
+    $keyAnotherBlock = 0;
+
+    while($keyAnotherBlock < count($tags)-1) {
+        $oneBlock = oneBlock($tags, $renameTags, $keyAnotherBlock);  // give last new block
+
+        //Block processing//
+
+        $keyAnotherBlock = keyAnoutherBlock($tags, $oneBlock, $keyAnotherBlock); // give new key
+
+        echo "\n".$tags[$keyAnotherBlock].$keyAnotherBlock;
+        break;
+    }
+
+}
+
+function startCreateFileRow($rowTags, $renameTags){
+    $f = fopen("form-row.php", 'w+');
+
+    ctartContentFile($rowTags, $renameTags);
+
+    fclose($f);
+}
+
+$templateTags = startCreateFile($html);  //in block_for_row file
+
+$renameTags = json_decode(file_get_contents ( "text_in_tag.json"));
+
+//var_dump($templateTags[1]);
+startCreateFileRow($templateTags[1], $renameTags);
--- create_Need_files/template.php
+++ create_Need_files/template.php
@@ -14,9 +14,29 @@
 <div class="chbbi popup-diagnostics__checkbox-wrapper">
 <div class="checkbox">
 <span class="checkbox__custom-checkbox-wrap">
+<input class="checkbox__custom-checkbox-input" type="checkbox" id="popup-diagnostics-agreement" name="popup-diagnostics-agreement" checked required>
+<span class="checkbox__custom-checkbox-inner">
+<svg class="checkbox__custom-checkbox-icon">
+<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="<?=SITE_TEMPLATE_PATH?>/icons/sprite.svg#check">
+</use>
+</svg>
+</span>
+<svg class="checkbox__custom-checkbox-border">
+<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="<?=SITE_TEMPLATE_PATH?>/icons/sprite.svg#check">
+</use>
+</svg>
+</span>
+<label class="checkbox__label" for="popup-diagnostics-agreement">Подтверждаю согласие на обработку моих
+<a class="checkbox__label-link js-personal-data-open">
+</a>
+</label>
 </div>
 </div>
 <div class="butbi popup-diagnostics__button-wrap">
+<?$arParams["BUTTON_TITLE"] = 'Отправить заявку';?>
+<button class="button popup-callback__button js-popup-callback-submit" name="iblock_submit" type="submit">
+<?= $arParams["BUTTON_TITLE"] ?>
+</button>
 </div>
 </div>
 </form>
